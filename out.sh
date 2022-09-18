4b38e5428431c1324fb6207b9b3d66262a799181
sreenath_reddy_g <gsreenath18@gmail.com>
2022-09-18 23:14:53 +0530

    script file added

---
+++ out.sh
@@ -0,0 +1,12 @@
+    1  history
+    2  history > out.sh
+    3  history
+    4  cat out.sh
+    5  cat out.sh
+    6  history
+    7  cat out.sh
+    8  history > out.sh
+    9  cat out.sh
+   10  clear
+   11  ls
+   12  history > out.sh
